ee34773af51f54ea70218b957ec26528b8897bba
pigfaceso <pigfaceso.work@gmail.com>
2026-09-12 13:45:33 +0700

    update

--- modules/desktop/obs-studio.nix
+++ modules/desktop/obs-studio.nix
@@ -5,7 +5,7 @@
     enable = true;
     plugins = with pkgs.obs-studio-plugins; [
       obs-advanced-masks
-      obs-move-transition
+      # obs-move-transition
     ];
   };
 }
--- modules/development/bun.nix
+++ modules/development/bun.nix
@@ -3,5 +3,5 @@
 {
   programs.bun = {
     enable = true;
-  }
+  };
 }
